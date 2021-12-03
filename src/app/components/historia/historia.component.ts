e8e7f69e25c0704ff9b9dffec7425cb82bccd11f
Adriana Lee <alee@est.utn.ac.cr>
2021-12-02 23:47:18 -0600

    Cambio historia

--- src/app/components/historia/historia.component.ts
+++ src/app/components/historia/historia.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historia.component.css']
 })
 export class HistoriaComponent implements OnInit {
+  
+  longText =  
+      `La empresa DreamOut nace legalmente constituida en el año 2006, 
+      luego de un sinnúmero de experiencias en ventas de tiquetes de avión y la necesidad de ser un 
+      ente constituido y respaldado por entidades que garanticen la funcionalidad y la imagen de empresa.
+
+      Siendo una empresa  constituida como persona natural, tuvo importantes alianzas con empresas de 
+      gran reconocimiento, donde se inicio como vendedor de tiques con familiares y amigos.
+
+      Posteriormente se trabajo en compañías aérea INTERCONTINENTAL DE AVIACION, 
+      la cual se pude aprender y tener más experiancia.
+  
+      Con las experiencias adquiridas, se empezo a conformar lod propios planes de tiques, creando un gran 
+      mercado. Hoy con la ayuda de todos se llevo a expandir la empresa con más de 20 años 
+      manteniendo una imagen de responsabilidad, cumplimento y calidad.`;
 
   constructor() { }
 
