b0fb64ec986345d7a2e1d64a3dd6fbcf4ed26bdf
Esteban Rivera <luigirp64@gmail.com>
2021-12-07 18:29:49 -0600

    Se arreglo crear factura

--- src/app/components/mantenimiento-factura/crear-factura/crearfactura.component.ts
+++ src/app/components/mantenimiento-factura/crear-factura/crearfactura.component.ts
@@ -9,133 +9,138 @@ import { facturaServise } from 'src/app/services/factura.service';
 @Component({
   selector: 'dreamOut-crearfactura',
   templateUrl: './crearfactura.component.html',
-  styleUrls: ['./crearfactura.component.css']
+  styleUrls: ['./crearfactura.component.css'],
 })
-
 export class CrearfacturaComponent implements OnInit {
-
   //Atributos del compomente
   idFactura: number = 0;
   textPantalla: string = 'Crear factura';
   isInsertar: boolean = true;
-  form:FormGroup;
-  factura = new factura;
+  form: FormGroup;
+  factura = new factura();
 
   //Se inyectan los objetos requerios y se crear el formulario
-  constructor(private facturaService: facturaServise,
-    private fb: FormBuilder, private router: Router, 
+  constructor(
+    private facturaService: facturaServise,
+    private fb: FormBuilder,
+    private router: Router,
     private _snackbar: MatSnackBar,
-    private activeRouter: ActivatedRoute) { 
-
+    private activeRouter: ActivatedRoute
+  ) {
     this.form = this.fb.group({
-      _id: ['', Validators.required],
       ID_Tiquete: ['', Validators.required],
       ID_Usuario: ['', Validators.required],
       fecha: ['', Validators.required],
       IVA: ['', Validators.required],
-      Total: ['', Validators.required]
+      Total: ['', Validators.required],
     });
   }
 
   ngOnInit(): void {
-
     //Cuando se inicializa el compomente de consulta si el ID fue enviado por parametro
-    this.activeRouter.params.subscribe((params: Params) => {      
+    this.activeRouter.params.subscribe((params: Params) => {
       console.log(params);
       this.idFactura = params['_id'];
 
-      //se consultan los datos de la factura 
-      if(this.idFactura !== undefined){
+      //se consultan los datos de la factura
+      if (this.idFactura !== undefined) {
         this.isInsertar = false;
-        this.textPantalla = "Modificar factura";
+        this.textPantalla = 'Modificar factura';
 
-        //se consultan los datos de la factura 
-        this.facturaService.get(this.idFactura)
-          .subscribe({
-            next: (res: any) => {
-              
-              this.factura = res;
-              this.form.setValue({_id: this.factura._id, 
-                                  ID_Tiquete: this.factura.ID_Tiquete, 
-                                  ID_Usuario: this.factura.ID_Usuario, 
-                                  fecha: this.factura.fecha,
-                                  IVA: this.factura.IVA,
-                                  Total: this.factura.Total});
+        //se consultan los datos de la factura
+        this.facturaService.get(this.idFactura).subscribe({
+          next: (res: any) => {
+            this.factura = res;
+            this.form.setValue({
+              ID_Tiquete: this.factura.ID_Tiquete,
+              ID_Usuario: this.factura.ID_Usuario,
+              fecha: this.factura.fecha,
+              IVA: this.factura.IVA,
+              Total: this.factura.Total,
+            });
 
-              console.log(this.factura);
+            console.log(this.factura);
 
-              this._snackbar.open('La factura fue cargada con exito, por favor verificar', '',{
+            this._snackbar.open(
+              'La factura fue cargada con exito, por favor verificar',
+              '',
+              {
                 duration: 5000,
                 horizontalPosition: 'center',
-                verticalPosition: 'bottom'
-              });
-            },
-            error: (e:any) => console.error(e)
+                verticalPosition: 'bottom',
+              }
+            );
+          },
+          error: (e: any) => console.error(e),
         });
         console.log('_id factura' + this.idFactura);
       }
     });
   }
-  
+
   //Método para modificar una factura
-  modificarFactura(): void{
+  modificarFactura(): void {
     const data = {
       _id: this.form.value._id,
       ID_Tiquete: this.form.value.ID_Tiquete,
       ID_Usuario: this.form.value.ID_Usuario,
       fecha: this.form.value.fecha,
       IVA: this.form.value.IVA,
-      Total: this.form.value.Total
+      Total: this.form.value.Total,
     };
 
     console.log(data);
 
-    this.facturaService.update(this.idFactura,data)
-      .subscribe({
-        next: (res: any) => {
-          this.form.reset;
-          console.log(res);
-          this.router.navigateByUrl('/dashboard/mantenimientoFactura');
+    this.facturaService.update(this.idFactura, data).subscribe({
+      next: (res: any) => {
+        this.form.reset;
+        console.log(res);
+        this.router.navigateByUrl('/dashboard/mantenimientoFactura');
 
-          this._snackbar.open('La factura fue modificada con exito, por favor verificar', '',{
+        this._snackbar.open(
+          'La factura fue modificada con exito, por favor verificar',
+          '',
+          {
             duration: 5000,
             horizontalPosition: 'center',
-            verticalPosition: 'bottom'
-          })
-          
-        },
-        error: (e:any) => console.error(e)
-      });
+            verticalPosition: 'bottom',
+          }
+        );
+      },
+      error: (e: any) => console.error(e),
+    });
   }
 
   //Método para guardar una nueva factura
-  saveFactura(): void{
+  saveFactura(): void {
     const data = {
-      _id: this.form.value._id,
+      _id: null,
       ID_Tiquete: this.form.value.ID_Tiquete,
       ID_Usuario: this.form.value.ID_Usuario,
       fecha: this.form.value.fecha,
       IVA: this.form.value.IVA,
-      Total: this.form.value.Total
+      Total: this.form.value.Total,
     };
 
-    console.log(data);
+    console.log('data in save factura',data);
 
-    this.facturaService.create(data)
-      .subscribe({
-        next: (res: any) => {
-          this.form.reset;
-          console.log(res);
-          this.router.navigateByUrl('/dashboard/mantenimientoFactura');
+    this.facturaService.create(data).subscribe({
+      next: (res: any) => {
+        this.form.reset;
+        console.log(res);
+        //this.router.navigateByUrl('/dashboard/mantenimientoFactura'); nesecita una ruta que exista en este proyecto
 
-          this._snackbar.open('La factura fue agregada con exito, por favor verificar', '',{
+        this._snackbar.open(
+          'La factura fue agregada con exito, por favor verificar',
+          '',
+          {
             duration: 5000,
             horizontalPosition: 'center',
-            verticalPosition: 'bottom'
-          })
-          
-        },
-        error: (e:any) => console.error(e)
-      });
+            verticalPosition: 'bottom',
+          }
+        );
+      },
+      error: (e: any) => console.error(e),
+    });
   }
 }
